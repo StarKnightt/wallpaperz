0063298b9921d0b90e055c011e0eec6cece9db62
StarKnightt <prasenjitt4e@gmail.com>
2025-02-17 13:37:04 +0530

    Done

--- app/auth.ts
+++ app/auth.ts
@@ -25,6 +25,11 @@ export const authOptions = {
     GitHub({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+      authorization: {
+        params: {
+          redirect_uri: 'https://wallpaperz.in/api/auth/callback/github'
+        }
+      }
     }),
     Google({
       clientId: process.env.GOOGLE_ID ?? "",
@@ -32,6 +37,7 @@ export const authOptions = {
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
   session: { strategy: "jwt" as const },
   pages: {
     signIn: '/auth/signin',
@@ -40,6 +46,18 @@ export const authOptions = {
   callbacks: {
     async redirect() {
       return 'https://wallpaperz.in'
+    },
+    async session({ session, token }: { session: Session, token: JWT }) {
+      if (token.accessToken) {
+        session.accessToken = token.accessToken
+      }
+      return session
+    },
+    async jwt({ token, account }: { token: JWT, account: any }) {
+      if (account?.access_token) {
+        token.accessToken = account.access_token
+      }
+      return token
     }
   }
 }
